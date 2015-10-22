ce5d1a3470238916c55fd58cabea2a1a765fe28f
Bart Vrancken <bart@abuse.io>
2015-10-22 20:59:17 +0200

    add spamexperts migrated from 3.1

---
+++ config/Spamexperts.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'parser' => [
+        'name'          => 'Spamexperts',
+        'enabled'       => true,
+        'report_file'   => false,
+        'sender_map'    => [
+            '/noreply@spamlogin.com/',
+        ],
+        'body_map'      => [
+            '/User-Agent:\ Spampanel/',
+            '/User-Agent:\ SpamExperts/',
+        ],
+    ],
+
+    'feeds' => [
+        'default' => [
+            'class'     => 'SPAM',
+            'type'      => 'Abuse',
+            'enabled'   => true,
+            'fields'    => [
+                //
+            ],
+            'filters'    => [
+                //
+            ],
+        ],
+
+    ],
+];
+
---
+++ src/Spamexperts.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AbuseIO\Parsers;
+
+class Spamexperts extends Parser
+{
+    /**
+     * Create a new Blocklistde instance
+     */
+    public function __construct($parsedMail, $arfMail)
+    {
+        parent::__construct($parsedMail, $arfMail, $this);
+    }
+
+    /**
+     * Parse attachments
+     * @return Array    Returns array with failed or success data
+     *                  (See parser-common/src/Parser.php) for more info.
+     */
+    public function parse()
+    {
+
+    }
+}
