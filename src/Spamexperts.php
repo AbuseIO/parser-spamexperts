6df9ebcdea21521d78d306f4cecc55f114b8e81b
Bart Vrancken <bart@abuse.io>
2015-12-30 12:44:02 +0100

    add docblocks

--- src/Spamexperts.php
+++ src/Spamexperts.php
@@ -2,10 +2,17 @@
 
 namespace AbuseIO\Parsers;
 
+/**
+ * Class Spamexperts
+ * @package AbuseIO\Parsers
+ */
 class Spamexperts extends Parser
 {
     /**
      * Create a new Spamexperts instance
+     *
+     * @param \PhpMimeMailParser\Parser $parsedMail phpMimeParser object
+     * @param array $arfMail array with ARF detected results
      */
     public function __construct($parsedMail, $arfMail)
     {
@@ -14,7 +21,7 @@ class Spamexperts extends Parser
 
     /**
      * Parse attachments
-     * @return Array    Returns array with failed or success data
+     * @return array    Returns array with failed or success data
      *                  (See parser-common/src/Parser.php) for more info.
      */
     public function parse()
